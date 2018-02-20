35d0bb1cd5c18fa01bec7a77ef85178574efff8f
Elena Lanigan <elenalanigan93@gmail.com>
2018-02-20 22:39:39 +0000

    Updates

--- app/views.py
+++ app/views.py
@@ -5,10 +5,13 @@ Created on 20 Feb 2018
 '''
 from flask import render_template
 from app import app
+import System_Info.main as si
 
 @app.route('/')
 def index():
+    print(si.main())
     returnDict = {}
     returnDict['user'] = 'Elena' # Feel free to put your name here!
     returnDict['title'] = 'Flask App'
+    returnDict['systeminfo'] = si.main()
     return render_template("index.html", **returnDict)
--- test.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 20 Feb 2018
-
-@author: elenalanigan
-'''
-from System_Info import __main__.main()
-
-print(main())
